b260323bf946466042dbb18c30be5ddaa596a51c
ESTOLEL <elijah.alixtair.estolas@oocl.com>
2022-09-01 14:11:04 +0800

    feat: add return for BUZZ when countOff parameter is a multiple of 5

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -4,11 +4,24 @@ public class FizzBuzz {
     String FIZZ = "Fizz";
     String BUZZ = "Buzz";
     String WHIZZ = "Whizz";
-    public String countOff(int firstNumber) {
+    public String countOff(int number) {
         String returnString = "";
-        if(firstNumber%3==0){
-            returnString = FIZZ;
-        }
+        returnString = addFizz(number, returnString);
+        returnString = addBuzz(number, returnString);
         return  returnString;
+    }
+
+    private String addBuzz(int number, String returnString) {
+        if(number %5==0){
+            returnString += BUZZ;
+        }
+        return returnString;
+    }
+
+    private String addFizz(int number, String returnString) {
+        if(number %3==0){
+            returnString += FIZZ;
+        }
+        return returnString;
     }
 }
