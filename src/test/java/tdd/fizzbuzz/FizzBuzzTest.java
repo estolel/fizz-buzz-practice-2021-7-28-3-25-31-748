c743a4898078e3c0840f96a613e4e21f9f751dc4
ESTOLEL <elijah.alixtair.estolas@oocl.com>
2022-09-01 14:11:51 +0800

    test: add test for WHIZZ return type

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -19,16 +19,17 @@ public class FizzBuzzTest {
         fizzBuzz = new FizzBuzz();
         int firstNumber = 3;
         int secondNumber = 5;
-//        int thirdNumber  = 21;
+        int thirdNumber  = 21;
 //        int fourthNumber = 105;
         //when
         String firstOutput = fizzBuzz.countOff(firstNumber);
         String secondOutput = fizzBuzz.countOff(secondNumber);
-//        String firstOutput = fizzBuzz.countOff(thirdNumber);
+        String thirdOutput = fizzBuzz.countOff(thirdNumber);
 //        String firstOutput = fizzBuzz.countOff(fourthNumber);
         //then
         assertEquals(FIZZ, firstOutput);
         assertEquals(BUZZ, secondOutput);
+        assertEquals(WHIZZ, thirdOutput);
 
     }
 
