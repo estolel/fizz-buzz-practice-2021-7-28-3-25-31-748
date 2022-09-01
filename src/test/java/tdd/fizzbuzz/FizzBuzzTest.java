4f485161337076effc1ad1c9c048f44cb2bdc207
ESTOLEL <elijah.alixtair.estolas@oocl.com>
2022-09-01 14:21:45 +0800

    test: split tests into more specific test methods

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -17,22 +17,36 @@ public class FizzBuzzTest {
     void should_return_output_number_or_fizzbuzzwhen_when_number_given_value() {
         //given
         fizzBuzz = new FizzBuzz();
-        int firstNumber = 3;
         int secondNumber = 5;
         int thirdNumber  = 7;
         int fourthNumber = 105;
+        int fifthNumber = 15;
+        int sixthNumber = 15;
         //when
-        String firstOutput = fizzBuzz.countOff(firstNumber);
         String secondOutput = fizzBuzz.countOff(secondNumber);
         String thirdOutput = fizzBuzz.countOff(thirdNumber);
         String fourthOutput = fizzBuzz.countOff(fourthNumber);
+
         //then
-        assertEquals(FIZZ, firstOutput);
         assertEquals(BUZZ, secondOutput);
         assertEquals(WHIZZ, thirdOutput);
         assertEquals(FIZZ+BUZZ+WHIZZ, fourthOutput);
 
     }
+
+    @Test
+    void should_return_fizz_when_count_off_given_multiple_of_3() {
+        //given
+
+        int firstNumber = 3;
+
+        //when
+        String firstOutput = fizzBuzz.countOff(firstNumber);
+
+        //then
+        assertEquals(FIZZ, firstOutput);
+    }
+
 
 
 
