1286bb0248202e21d8e4b85f084db11cd13d0c8d
ESTOLEL <elijah.alixtair.estolas@oocl.com>
2022-09-01 14:08:38 +0800

    test: add test for BUZZ return type for countOff

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -18,16 +18,17 @@ public class FizzBuzzTest {
         //given
         fizzBuzz = new FizzBuzz();
         int firstNumber = 3;
-//        int secondNumber = 5;
+        int secondNumber = 5;
 //        int thirdNumber  = 21;
 //        int fourthNumber = 105;
         //when
         String firstOutput = fizzBuzz.countOff(firstNumber);
-//        String secondOutput = fizzBuzz.countOff(secondNumber);
+        String secondOutput = fizzBuzz.countOff(secondNumber);
 //        String firstOutput = fizzBuzz.countOff(thirdNumber);
 //        String firstOutput = fizzBuzz.countOff(fourthNumber);
         //then
         assertEquals(FIZZ, firstOutput);
+        assertEquals(BUZZ, secondOutput);
 
     }
 
